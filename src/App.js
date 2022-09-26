3786d994507f38e6e402583877a483fbe90a2c3e
muharrem gem <muharremgem28@gmail.com>
2022-09-26 18:41:49 +0300

    useEfect

--- src/App.js
+++ src/App.js
@@ -5,35 +5,36 @@ import City from "./City";
 
 function App() {
   const key = "8f4c38a691e3b6d6ec2f65ee89b330b0";
-  const [search, setSearch] = useState("");
-  const [city, setCity] = useState();
+  const [search, setSearch] = useState("ankara");
+  const [city, setCity] = useState([]);
+  const [isClick, setİsClick] = useState(false);
+  
 
-  useEffect(() => {
-    async function getApi() {
-      try {
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
-        );
-        console.log(response);
-        setCity(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+  async function getApi() {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
+      );
+      console.log(response.data);
+      setCity(response.data);
+      setİsClick(true);
+    } catch (error) {
+      console.error(error);
     }
-    getApi();
-  }, [search]);
+  }
   console.log(search);
   return (
     <div className="App">
       <div>
-        <div class="mb-3 pt-0">
+        <div className="mb-3 pt-0 bg-gray-200 w-full m-auto mt-8">
           <input
             onChange={(e) => setSearch(e.target.value)}
             type="text"
-            placeholder="Bir İl Giriniz.."
+            placeholder={search ? search : "ilGiriniz"}
             class="mt-5 px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-lime-500 rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-[20rem]"
           />
-          <City city={city} />
+          <button onClick={() => getApi()}>Submit</button>
+          {isClick && <City city={city} />}
         </div>
       </div>
     </div>
