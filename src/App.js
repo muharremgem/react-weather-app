2b41916abfa16ea4158729a7792dafed73bfe9a9
muharrem gem <muharremgem28@gmail.com>
2022-09-26 17:47:22 +0300

    Cİty

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
+import City from "./City";
 
 function App() {
   const key = "8f4c38a691e3b6d6ec2f65ee89b330b0";
@@ -20,19 +21,20 @@ function App() {
       }
     }
     getApi();
-  }, []);
+  }, [search]);
   console.log(search);
   return (
     <div className="App">
       <div>
-        <div className="mb-3 pt-8">
-          input
+        <div class="mb-3 pt-0">
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            placeholder="Bir İl Giriniz.."
+            class="mt-5 px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-lime-500 rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-[20rem]"
+          />
+          <City city={city} />
         </div>
-        <input
-          onChange={(e) => setSearch(e.target.value)}
-          type="text"
-          className="py-1 mt-6 bg-lime-500 text-black"
-        />
       </div>
     </div>
   );
